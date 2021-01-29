10c0bb86d6cebd7e914a8bb8cc47beffa3c63e09
Subodh <irshad@subcodevs.com>
2021-01-29 16:59:08 +0530

    code from part-1 video

---
+++ UserProfile/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UserProfile
+//
+//  Created by Irshad Ahmed on 29/01/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
